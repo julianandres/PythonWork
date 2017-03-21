ff212c17f0ee7cd22307c443cabfc6ac19de37ba
julianandres <julianbolanos@unicauca.edu.co>
2017-03-21 15:55:46 -0500

    commit wifil

--- colorMap.py
+++ colorMap.py
@@ -21,21 +21,25 @@ def colorGradeBGRMatPlot(ndvi,nombre,multiplo,pathproject,tipofoto):
 
 	#cmapvals=cmap._segmentdata
 	
-	fig, ax = plt.subplots(figsize=(20, 20))
+	fig, ax = plt.subplots()
+	fig.subplots_adjust(bottom=0,left=0,top=1,right=1)
 	del ndvi
 	im = ax.imshow(data, cmap=cmap,vmin=0,vmax=vmax)
+	ax.set_axis_off()
+	ax.get_xaxis().set_visible(False)
+	ax.get_yaxis().set_visible(False)
 	print "hola"
 	#del cmap
 	#im = ax.pcolor(data, cmap=cmap, vmin=0, vmax=vmax)
-	cbar = fig.colorbar(im, ticks=[0, 1, 2], orientation='horizontal')
-	cbar.ax.set_xticklabels(['-1', '0', '1',])  # horizontal colorbar
+	#cbar = fig.colorbar(im, ticks=[0, 1, 2], orientation='vertical')
+	#cbar.ax.set_xticklabels(['-1', '0', '1',])  # horizontal colorbar
 	print "syse"
 	print sys.getsizeof(im)
 	print sys.getsizeof(fig)
 	print sys.getsizeof(ax)
 	print sys.getsizeof(data)
 	#plt.show()	
-	plt.savefig(pathproject+"ResultadosNDVI/ResMatPlot/"+tipofoto+nombre+"MatploTNDVI.jpg")	
+	plt.savefig(pathproject+"ResultadosNDVI/ResMatPlot/"+tipofoto+nombre+"MatploTNDVI.jpg",bbox_inches='tight', pad_inches=0,dpi=200)	
 	fig.clf()
 	plt.close()
 	
